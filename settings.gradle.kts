efe7b968bdaf9f921e43d25cb27a04e20d6bff84
drnopoh2810-spec <dr.nopoh2810@gmail.com>
2026-04-15 15:35:45 +0200

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SMS Payment Gateway"
+include(":app")
